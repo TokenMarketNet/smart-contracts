a46cd27c15644556b8f8ef73741b8a39b7278144
Ville Sundell <development@solarius.fi>
2017-06-05 19:59:20 +0300

    AllocatedCrowdsale: Replaced if-else with one "return"
    According to https://medium.com/@maraoz/f1d742b94756:
    "Lines 28 to 32 of AllocatedCrowdsale.sol could be replaced
    by return tokenAmount > getTokensLeft();."

--- contracts/AllocatedCrowdsale.sol
+++ contracts/AllocatedCrowdsale.sol
@@ -25,11 +25,7 @@ contract AllocatedCrowdsale is Crowdsale {
    * Called from invest() to confirm if the curret investment does not break our cap rule.
    */
   function isBreakingCap(uint weiAmount, uint tokenAmount, uint weiRaisedTotal, uint tokensSoldTotal) constant returns (bool limitBroken) {
-    if(tokenAmount > getTokensLeft()) {
-      return true;
-    } else {
-      return false;
-    }
+    return tokenAmount > getTokensLeft();
   }
 
   /**
