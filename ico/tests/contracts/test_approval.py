5c1d271daaed35741abc631f1c87d110e21430a1
Ville Sundell <development@solarius.fi>
2017-06-25 15:47:09 +0300

    test_approval.py: TEMPORARILY commenting out approve tests
    It turned out that subApproval and addApproval are now removed.
    Need to redesign approve tests.

--- ico/tests/contracts/test_approval.py
+++ ico/tests/contracts/test_approval.py
@@ -1,26 +1,27 @@
 """Approval race condition mitigation."""
+"""Commented out temporarily"""
 from web3.contract import Contract
 
 
-def test_increase_approval(released_token: Contract, customer: str, empty_address: str, allowed_party):
-    """Increase approval."""
+#def test_increase_approval(released_token: Contract, customer: str, empty_address: str, allowed_party):
+#    """Increase approval."""
+#
+#    token = released_token
+#    amount = 5000
+#    change = 1000
+#    assert token.call().allowance(customer, allowed_party) == 0
+#    token.transact({"from": customer}).approve(allowed_party, amount)
+#    token.transact({"from": customer}).addApproval(allowed_party, change)
+#    assert token.call().allowance(customer, allowed_party) == amount + change
 
-    token = released_token
-    amount = 5000
-    change = 1000
-    assert token.call().allowance(customer, allowed_party) == 0
-    token.transact({"from": customer}).approve(allowed_party, amount)
-    token.transact({"from": customer}).addApproval(allowed_party, change)
-    assert token.call().allowance(customer, allowed_party) == amount + change
 
+#def test_decrease_approval(released_token: Contract, customer: str, empty_address: str, allowed_party):
+#    """Decrease approval."""
 
-def test_decrease_approval(released_token: Contract, customer: str, empty_address: str, allowed_party):
-    """Decrease approval."""
-
-    token = released_token
-    amount = 5000
-    change = 1000
-    assert token.call().allowance(customer, allowed_party) == 0
-    token.transact({"from": customer}).approve(allowed_party, amount)
-    token.transact({"from": customer}).subApproval(allowed_party, change)
-    assert token.call().allowance(customer, allowed_party) == amount - change
+#    token = released_token
+#    amount = 5000
+#    change = 1000
+#    assert token.call().allowance(customer, allowed_party) == 0
+#    token.transact({"from": customer}).approve(allowed_party, amount)
+#    token.transact({"from": customer}).subApproval(allowed_party, change)
+#    assert token.call().allowance(customer, allowed_party) == amount - change
