5f33d6c0d24fd1230156306ac29407a68ea7bdf2
Roman Storm <rstormsf@gmail.com>
2017-08-25 14:12:23 -0700

    update solidity version for flat_pricing.sol

--- contracts/FlatPricing.sol
+++ contracts/FlatPricing.sol
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, version 2.0: https://github.com/TokenMarketNet/ico/blob/master/LICENSE.txt
  */
 
-pragma solidity ^0.4.6;
+pragma solidity ^0.4.12;
 
 import "./PricingStrategy.sol";
 import "./SafeMathLib.sol";
