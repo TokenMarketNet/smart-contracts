b73ab9e4cb4f3250d2ce00b1beeab805f79e0a45
sevenflash <sevenflash@yandex.ru>
2017-09-10 08:40:55 +0300

    fix typo

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -288,7 +288,7 @@ contract Crowdsale is Haltable {
    * Allow anonymous contributions to this crowdsale.
    */
   function invest(address addr) public payable {
-    if(requireCustomerId) throw; // Crowdsale needs to track partipants for thank you email
+    if(requireCustomerId) throw; // Crowdsale needs to track participants for thank you email
     if(requiredSignedAddress) throw; // Crowdsale allows only server-side signed participants
     investInternal(addr, 0);
   }
