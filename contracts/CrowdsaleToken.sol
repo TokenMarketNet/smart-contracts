00f073f594ef24499ff580a4eb325b232a543410
Tim Loh <timloh@Tims-MacBook-Pro.local>
2017-09-16 02:15:13 +0800

    Eliminate throw from CrowdsaleToken.sol.

--- contracts/CrowdsaleToken.sol
+++ contracts/CrowdsaleToken.sol
@@ -47,6 +47,9 @@ contract CrowdsaleToken is ReleasableToken, MintableToken, UpgradeableToken {
   function CrowdsaleToken(string _name, string _symbol, uint _initialSupply, uint _decimals, bool _mintable)
     UpgradeableToken(msg.sender) {
 
+    // Cannot create a token without supply and no minting
+    require(_mintable || _initialSupply != 0);
+
     // Create any address, can be transferred
     // to team multisig via changeOwner(),
     // also remember to call setUpgradeMaster()
@@ -69,9 +72,6 @@ contract CrowdsaleToken is ReleasableToken, MintableToken, UpgradeableToken {
     // No more new supply allowed after the token creation
     if(!_mintable) {
       mintingFinished = true;
-      if(totalSupply == 0) {
-        throw; // Cannot create a token without supply and no minting
-      }
     }
   }
 
