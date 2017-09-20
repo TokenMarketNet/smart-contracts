ab015a8f38885e5aa8bd322b8aa162c41884a492
Ville Sundell <development@solarius.fi>
2017-09-20 12:07:32 +0300

    Recoverable: Created a contract which can return extra tokens from a contract
    By inheriting this contract, contract owner can invoke
    recoverTokens(ERC20Basic) to recover extra tokens to owners address, then the
    owner can manually send the tokens back to their rightful owner.
    
    recoverTokens() invokes tokensToBeReturned(), which can be overwritten by the
    super contract in those cases where some tokens should be left to the contract.
    
    Because our needs are unique, 3rd party code was not used:
    We need a mechanism which is simple, and reusable between our contracts.

---
+++ contracts/Recoverable.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.13;
+
+import "zeppelin/contracts/ownership/Ownable.sol";
+import "zeppelin/contracts/token/ERC20Basic.sol";
+
+contract Recoverable is Ownable {
+
+  /// @dev Empty constructor (for now)
+  function Recoverable() {
+  }
+
+  /// @dev This will be invoked by the owner, when owner wants to rescue tokens
+  /// @param token Token which will we rescue to the owner from the contract
+  function recoverTokens(ERC20Basic token) onlyOwner public {
+    token.transfer(owner, tokensToBeReturned(token));
+  }
+
+  /// @dev Interface function, can be overwritten by the superclass
+  /// @param token Token which balance we will check and return
+  /// @return The amount of tokens (in smallest denominator) the contract owns
+  function tokensToBeReturned(ERC20Basic token) public returns (uint) {
+    return token.balanceOf(this);
+  }
+}
