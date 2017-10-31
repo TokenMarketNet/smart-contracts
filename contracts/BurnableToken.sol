b7cec2457ac98e8d93bd6504f601a14db14cac3c
Mikko Ohtamaa <mikko@opensourcehacker.com>
2017-10-31 16:47:37 -0500

    Add faux Transfer event to burning as we do not have a standard Burn event yet.

--- contracts/BurnableToken.sol
+++ contracts/BurnableToken.sol
@@ -10,6 +10,7 @@ import "./StandardTokenExt.sol";
 
 contract BurnableToken is StandardTokenExt {
 
+  // @notice An address for the transfer event where the burned tokens are transferred in a faux Transfer event
   address public constant BURN_ADDRESS = 0;
 
   /** How many tokens we burned */
@@ -24,5 +25,10 @@ contract BurnableToken is StandardTokenExt {
     balances[burner] = balances[burner].sub(burnAmount);
     totalSupply = totalSupply.sub(burnAmount);
     Burned(burner, burnAmount);
+
+    // Inform the blockchain explores that track the
+    // balances only by a transfer event that the balance in this
+    // address has decreased
+    Transfer(burner, BURN_ADDRESS, burnAmount);
   }
 }
