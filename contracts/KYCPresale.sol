1859fc9b835a84e5bda49df3edf6987271f1bf28
Ville Sundell <development@solarius.fi>
2018-01-25 00:44:26 +0200

    KYCPresale: Initial implementation of KYC enabled presale process with tier support
    We need a KYC enabled presale smart contract with tiering, which will just
    forward the ethers to a multisig, and records an event telling who should
    receive and how many tokens, and on which tier.
    
    Very basic functionality, and this commit does not include tests yet.

---
+++ contracts/KYCPresale.sol
@@ -0,0 +1,93 @@
+import "./CrowdsaleBase.sol";
+import "./AllocatedCrowdsaleMixin.sol";
+import "./KYCPayloadDeserializer.sol";
+
+/**
+ * A crowdsale that allows only signed payload with server-side specified buy in limits.
+ *
+ *
+ * The token distribution happens as in the allocated crowdsale.
+ *
+ */
+contract KYCPresale is AllocatedCrowdsaleMixin, KYCPayloadDeserializer {
+
+  /* Server holds the private key to this address to decide if the AML payload is valid or not. */
+  address public signerAddress;
+
+  /* A new server-side signer key was set to be effective */
+  event SignerChanged(address signer);
+
+
+  /* Prebuy made */
+  event PreBuy(address investor, uint weiAmount, uint tokenAmount, uint128 customerId, bytes32 pricingTierId);
+
+  /**
+   * Constructor.
+   */
+  function KYCPresale(address _token, PricingStrategy _pricingStrategy, address _multisigWallet, uint _start, uint _end, uint _minimumFundingGoal, address _beneficiary) CrowdsaleBase(_token, _pricingStrategy, _multisigWallet, _start, _end, _minimumFundingGoal) AllocatedCrowdsaleMixin(_beneficiary) {
+
+  }
+
+  /**
+   * A token purchase with anti-money laundering
+   *
+   * ©return tokenAmount How many tokens where bought
+   */
+  function buyWithKYCData(bytes dataframe, uint8 v, bytes32 r, bytes32 s) public payable returns(uint tokenAmount) {
+    var (whitelistedAddress, customerId, minETH, maxETH, pricingTierId) = getKYCPresalePayload(dataframe);
+    uint multiplier = 10 ** 18;
+    address receiver = msg.sender;
+    uint weiAmount = msg.value;
+
+    // Determine if it's a good time to accept investment from this participant
+    if(getState() == State.PreFunding) {
+      // Are we whitelisted for early deposit
+      if(!earlyParticipantWhitelist[receiver]) {
+        throw;
+      }
+    } else if(getState() == State.Funding) {
+      // Retail participants can only come in when the crowdsale is running
+      // pass
+    } else {
+      // Unwanted state
+      throw;
+    }
+
+    if(investedAmountOf[receiver] == 0) {
+       // A new investor
+       investorCount++;
+    }
+
+    // Update investor
+    investedAmountOf[receiver] = investedAmountOf[receiver].plus(weiAmount);
+
+    // Update totals
+    weiRaised = weiRaised.plus(weiAmount);
+
+    if(pricingStrategy.isPresalePurchase(receiver)) {
+        presaleWeiRaised = presaleWeiRaised.plus(weiAmount);
+    }
+
+    // Check that we did not bust the cap
+    require(!isBreakingCap(weiAmount, tokenAmount, weiRaised, tokensSold));
+
+    require(investedAmountOf[msg.sender] >= minETH * multiplier / 10000);
+    require(investedAmountOf[msg.sender] <= maxETH * multiplier / 10000);
+
+    // Pocket the money, or fail the crowdsale if we for some reason cannot send the money to our multisig
+    if(!multisigWallet.send(weiAmount)) throw;
+
+    // Tell us invest was success
+    PreBuy(receiver, weiAmount, tokenAmount, customerId, pricingTierId);
+
+    return tokenAmount;
+  }
+
+  /// @dev This function can set the server side address
+  /// @param _signerAddress The address derived from server's private key
+  function setSignerAddress(address _signerAddress) onlyOwner {
+    signerAddress = _signerAddress;
+    SignerChanged(signerAddress);
+  }
+
+}
