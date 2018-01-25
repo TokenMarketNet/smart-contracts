da54f6e34a8bc52cc81dcf6404f39b0d9ac2d533
Ville Sundell <development@solarius.fi>
2018-01-25 17:01:48 +0200

    KYCPresale: Created tests
    Created tests based on the earlier test_kyccrowdsale.py and
    modified test_kyc_payload_deserializing.py and kyc.py.

--- ico/kyc.py
+++ ico/kyc.py
@@ -27,3 +27,30 @@ def pack_kyc_dataframe(whitelisted_address: str, customer_id: UUID, min_eth_10k:
     data = addr_b + customer_b + min_b + max_b
     assert len(data) == 44, "Got length: {}".format(len(data))
     return data
+
+def pack_kyc_presale_dataframe(whitelisted_address: str, customer_id: UUID, min_eth_10k: int, max_eth_10k: int, pricing_info: int) -> bytes:
+    """Pack KYC presale information to the smart contract.
+
+    See KYCPayloadDeserializer for the matching Solidity code.
+
+    :param whitelisted_address: Must be whitelisted address in a Ethereum checksummed format
+    :param customer_id: Customer id as UUIDv8
+    :param min_eth: Min investment for this customer. Expressed as the parts of 1/10000.
+    :param max_eth: Max investment for this customer. Expressed as the parts of 1/10000.
+    :param pricing_info: Tier identifier.
+    :return:
+    """
+    assert is_checksum_address(whitelisted_address)
+    assert isinstance(customer_id, UUID)
+    assert type(min_eth_10k) == int
+    assert type(max_eth_10k) == int
+    assert type(pricing_info) == int
+    addr_value = int(whitelisted_address, 16)
+    addr_b = addr_value.to_bytes(20, byteorder="big")  # Ethereum address is 20 bytes
+    customer_b = customer_id.bytes
+    min_b = min_eth_10k.to_bytes(4, byteorder="big")
+    max_b = max_eth_10k.to_bytes(4, byteorder="big")
+    pricing_data = pricing_info.to_bytes(32, byteorder="big")
+    data = addr_b + customer_b + min_b + max_b + pricing_data
+    assert len(data) == 76, "Got length: {}".format(len(data))
+    return data
--- ico/tests/contracts/test_kyc_payload_deserializing.py
+++ ico/tests/contracts/test_kyc_payload_deserializing.py
@@ -5,6 +5,7 @@ from eth_utils import to_normalized_address, to_checksum_address
 from web3.contract import Contract
 
 from ico.kyc import pack_kyc_dataframe
+from ico.kyc import pack_kyc_presale_dataframe
 
 
 @pytest.fixture()
@@ -39,3 +40,20 @@ def test_roundtrip_kyc_data(kyc_deserializer, whitelisted_address):
     assert tuple_value[2] == 1000
     assert tuple_value[3] == 99990000
 
+def test_roundtrip_kyc_presale_data(kyc_deserializer, whitelisted_address):
+    """We correctly encode data in Python side and decode it back in the smart contract."""
+
+    customer_id = uuid.uuid4()
+    dataframe = pack_kyc_presale_dataframe(whitelisted_address, customer_id, int(0.1 * 10000), int(9999 * 10000), 123)
+    tuple_value = kyc_deserializer.call().getKYCPresalePayload(dataframe)
+
+    #
+    # Check that the output looks like what we did expect
+    #
+
+    assert tuple_value[0].lower() == whitelisted_address.lower()
+    # Do a raw integer comparison, because web3.py and UUID disagree about left padding zeroes
+    assert tuple_value[1] == customer_id.int
+    assert tuple_value[2] == 1000
+    assert tuple_value[3] == 99990000
+    assert tuple_value[4] == 123
