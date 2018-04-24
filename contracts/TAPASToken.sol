23f47759f084e8d50bb636900ececb3c18dbf384
Ville Sundell <development@solarius.fi>
2018-04-24 12:53:51 +0300

    TAPAS: NatSpec commenting for the TAPASToken
    Creating NatSpec documentation for the actual TAPASToken.

--- contracts/TAPASToken.sol
+++ contracts/TAPASToken.sol
@@ -1,7 +1,7 @@
 /**
  * This smart contract code is Copyright 2017 TokenMarket Ltd. For more information see https://tokenmarket.net
- * Author: Ville Sundell <ville at tokenmarket.net>
  * Licensed under the Apache License, version 2.0: https://github.com/TokenMarketNet/ico/blob/master/LICENSE.txt
+ * NatSpec is used intentionally to cover also other than public functions
  */
 
 pragma solidity ^0.4.18;
@@ -11,16 +11,24 @@ import "./CheckpointToken.sol";
 import "./Recoverable.sol";
 import "zeppelin/contracts/ownership/Whitelist.sol";
 
-/// @dev TAPAS announcement interface
+/**
+ * @dev Interface for general TAPAS announcements.
+ *
+ * TAPASAnnouncements can be for instance for dividend sharing, voting, or
+ * just for general announcements.
+ */
 interface TAPASAnnouncement {
   function announcementName() public view returns (bytes32);
   function announcementURI() public view returns (bytes32);
   function announcementType() public view returns (uint256);
 }
 
+/**
+ * @author TokenMarket /  Ville Sundell <ville at tokenmarket.net>
+ */
 contract TAPASToken is CheckpointToken, Whitelist, Recoverable {
 
-  // TAPAS specific
+  /** TAPAS specific events **/
   event Issued(address indexed to, uint256 value);
   event Burned(address indexed burner, uint256 value);
   event Forced(address indexed from, address indexed to, uint256 value);
@@ -29,21 +37,60 @@ contract TAPASToken is CheckpointToken, Whitelist, Recoverable {
 
   address[] public announcements;
 
+  /**
+   * @dev Contructor to create TAPASToken, and subsequent CheckpointToken.
+   *
+   * CheckpointToken will be created with hardcoded 18 decimals.
+   *
+   * @param _name Initial name of the token
+   * @param _symbol Initial symbol of the token
+   */
   function TAPASToken(string _name, string _symbol) CheckpointToken(_name, _symbol, 18) public {
 
   }
 
+  /**
+   * @dev Function to announce TAPASAnnouncements.
+   *
+   * TAPASAnnouncements can be for instance for dividend sharing, voting, or
+   * just for general announcements.
+   *
+   * Instead of storing the announcement details, we just broadcast them as an
+   * event, and store only the address.
+   *
+   * @param announcement Address of TAPASAnnouncement
+   */
   function announce(TAPASAnnouncement announcement) external onlyWhitelisted {
     announcements.push(announcement);
     Announced(address(announcement), announcement.announcementType(), announcement.announcementName(), announcement.announcementURI());
   }
 
+  /**
+   * @dev Function to forcefully transfer tokens from A to B by board decission
+   *
+   * This must be implemented carefully, since this is a very critical part
+   * to ensure investor safety.
+   *
+   * This is intended to be called by the BAC (The Board), hence the whitelisting.
+   *
+   * @param from Address of the account to confisticate the tokens from
+   * @param to Address to deposit the confisticated token to
+   * @param value amount of tokens to be confisticated
+   */
   function forceTransfer(address from, address to, uint256 value) external onlyWhitelisted {
     transferInternal(from, to, value);
 
     Forced(from, to, value);
   }
 
+  /**
+   * @dev Issue new tokens to the board by a board decission
+   *
+   * Issue new tokens. This is intended to be called by the BAC (The Board),
+   * hence the whitelisting.
+   *
+   * @param value Token amount to issue
+   */
   function issueTokens(uint256 value) external onlyWhitelisted {
     address issuer = msg.sender;
     uint256 zeroBalance = balanceOf(address(0)); /// TODO: Rename!
@@ -56,8 +103,15 @@ contract TAPASToken is CheckpointToken, Whitelist, Recoverable {
     Issued(issuer, value);
   }
 
+  /**
+   * @dev Burn tokens from contract's own balance by a board decission
+   *
+   * Burn tokens from contract's own balance to prevent accidental burnings.
+   * This is intended to be called by the BAC (The Board), hence the whitelisting.
+   *
+   * @param value Token amount to burn from this contract's balance
+   */
   function burnTokens(uint256 value) external onlyWhitelisted {
-    // Burning only tokens sent to this contract, this way we avoid causing problems to other shareholders by accident
     address burner = address(this);
     uint256 burnerBalance = balanceOf(burner);
     uint256 totalSupplyNow = totalSupply();
@@ -70,13 +124,16 @@ contract TAPASToken is CheckpointToken, Whitelist, Recoverable {
   }
 
   /**
-  * Whitelisted can update token information here.
+  * @dev Whitelisted users (The Board, BAC) can update token information here.
   *
   * It is often useful to conceal the actual token association, until
   * the token operations, like central issuance or reissuance have been completed.
   *
   * This function allows the token owner to rename the token after the operations
   * have been completed and then point the audience to use the token contract.
+  *
+  * @param _name New name of the token
+  * @param _symbol New symbol of the token
   */
   function setTokenInformation(string _name, string _symbol) external onlyWhitelisted {
     name = _name;
