11cc768637059b1d9145806f66507e984a39a123
Mikko Ohtamaa <mikko@opensourcehacker.com>
2018-05-16 14:27:45 -0400

    Revert "ERC827: Support for ERC827 tokens, including basic acceptance tests"
    This reverts commit ec7e9aae21b3d21b2bb4b658a9dc1ec88a6be93d.

--- contracts/StandardTokenExt.sol
+++ contracts/StandardTokenExt.sol
@@ -7,7 +7,6 @@
 pragma solidity ^0.4.14;
 
 import "zeppelin/contracts/token/ERC20/StandardToken.sol";
-import "zeppelin/contracts/token/ERC827/ERC827Token.sol";
 import "./Recoverable.sol";
 
 
@@ -17,7 +16,7 @@ import "./Recoverable.sol";
  * @notice Interface marker is used by crowdsale contracts to validate that addresses point a good token contract.
  *
  */
-contract StandardTokenExt is StandardToken, ERC827Token, Recoverable {
+contract StandardTokenExt is StandardToken, Recoverable {
 
   /* Interface declaration */
   function isToken() public constant returns (bool weAre) {
