3f04a03a3f394879110e1906ec6694f9a3495351
Ville Sundell <development@solarius.fi>
2018-07-20 19:26:19 +0300

    TokenVault: renamed getClaimableAmount() to getCurrentlyClaimableAmount()
    In order to make querying the amount left as user friendly and intuitive as
    possible (when using tools like Etherscan), renamed the function once again
    per review feedback. TokenVault.balances(address) can be used to query the
    intended total balance, as always.
    
    So, now getCurrentlyClaimableAmount() is returning only the token amount
    user can claim _now_. 0 if they can't.

--- contracts/TokenVault.sol
+++ contracts/TokenVault.sol
@@ -192,21 +192,22 @@ contract TokenVault is Ownable, Recoverable {
   /// @dev Check how many tokens "investor" can claim
   /// @param investor Address of the investor
   /// @return uint How many tokens the investor can claim now
-  function getClaimableAmount(address investor) public constant returns (uint claimableAmount) {
+  function getCurrentlyClaimableAmount(address investor) public constant returns (uint claimableAmount) {
     uint maxTokensLeft = balances[investor] - claimed[investor];
-    uint sinceLastClaim = lastClaimedAt[investor];
-    uint maxClaim;
+
+    if (now < freezeEndsAt) {
+      return 0;
+    }
 
     if (tokensPerSecond > 0) {
-      if (sinceLastClaim == 0) {
-        sinceLastClaim = freezeEndsAt;
+      uint previousClaimAt = lastClaimedAt[investor];
+      uint maxClaim;
+
+      if (previousClaimAt == 0) {
+        previousClaimAt = freezeEndsAt;
       }
 
-      if (now < freezeEndsAt) {
-        return 0;
-      }
-
-      maxClaim = (now - sinceLastClaim) * tokensPerSecond;
+      maxClaim = (now - previousClaimAt) * tokensPerSecond;
 
       if (maxClaim > maxTokensLeft) {
         return maxTokensLeft;
@@ -236,7 +237,7 @@ contract TokenVault is Ownable, Recoverable {
       throw;
     }
 
-    uint amount = getClaimableAmount(investor);
+    uint amount = getCurrentlyClaimableAmount(investor);
     require (amount > 0);
 
     lastClaimedAt[investor] = now;
