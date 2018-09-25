a1c05a582be26350ef95916064a95298172357b0
Ville Sundell <development@solarius.fi>
2018-09-25 12:11:22 +0300

    TAPAS: Added freeze() to MockTransactionAgent
    This is for testing only: we can freeze all transactions.

--- security-token/tests/MockTransactionAgent.sol
+++ security-token/tests/MockTransactionAgent.sol
@@ -3,11 +3,19 @@ pragma solidity ^0.4.18;
 import "../transactionAgent.sol";
 
 contract MockTransactionAgent is transactionAgent {
-    function MockTransactionAgent() {
-      // This is here for our verification code only
-    }
+  bool frozen = false;
 
-    function verify(address from, address to, uint256 value) public returns (uint256 newValue) {
-      return 1;
-    }
+  function MockTransactionAgent() {
+    // This is here for our verification code only
+  }
+
+  function freeze() public {
+    frozen = true;
+  }
+
+  function verify(address from, address to, uint256 value) public returns (uint256 newValue) {
+    require(frozen == false);
+
+    return 1;
+  }
 }
