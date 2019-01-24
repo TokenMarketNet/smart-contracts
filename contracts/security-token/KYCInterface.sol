dca323e205de9d0dcccc49dedc2c8c47f0aafe13
Ville Sundell <development@solarius.fi>
2019-01-24 13:43:00 +0200

    KYCInterface: Simplified the interface specification
    Only function needed is isWhitelisted(), every other function can be
    implementation specific.

--- contracts/security-token/KYCInterface.sol
+++ contracts/security-token/KYCInterface.sol
@@ -3,9 +3,5 @@ pragma solidity ^0.4.18;
 interface KYCInterface {
     event Whitelisted(address who, uint128 nonce);
 
-    function whitelistUser(address who, uint128 nonce) external;
-
-    function whitelistMe(uint128 nonce, uint8 v, bytes32 r, bytes32 s) external;
-
     function isWhitelisted(address who) external view returns(bool);
 }
